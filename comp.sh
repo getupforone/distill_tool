d8441bf973786ecb670080c44dcbef78973b5796
giilkwon <getupforone@gmail.com>
2020-08-29 20:47:12 +0900

    daily commit

--- comp.sh
+++ comp.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 for d in */;do
-   tar -cvJf "${d%/}.tar.xz" "$d";
+   for dd in $d;do
+	echo dd is $dd
+   	tar -cvf - "${dd%/}" | xz -9e -T8 -c > "${dd%/}.tar.xz" ;
+   done
 done
 
 #for d in "*.tar";do echo $d;echo $d; xz -9e --threads=16 -c -> "${d}.xz"; done
